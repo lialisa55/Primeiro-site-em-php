cf998f7c8a4acc5ac8b273cfe9d6a50dc51bb12f
Nathalia Monalisa <119463033+lialisa55@users.noreply.github.com>
2023-03-03 21:25:57 -0300

    added math page

---
+++ matematica.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <title>Recebendo parâmetros</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+            $num1 = 3;
+            $num2 = 2;
+            $soma = $num1 + $num2;
+            $subtracao = $num1 - $num2;
+            $divisao = $num1 / $num2;
+            $resto = $num1 % $num2;
+            echo "<table border ='1'>"
+            echo "<tr>";
+                echo "<td>" . $num1 . " + " . $num2 . " = </td>";
+                echo "<td>" . $soma . "</td>"
+            echo "</tr>"
+            echo "<tr>";
+                echo "<td>" . $num1 . " - " . $num2 . " = </td>";
+                echo "<td>" . $subtracao . "</td>"
+            echo "</tr>"
+            echo "<tr>";
+                echo "<td>" . $num1 . " / " . $num2 . " = </td>";
+                echo "<td>" . $divisao . "</td>"
+            echo "</tr>"
+            echo "<tr>";
+                echo "<td>" . $num1 . " % " . $num2 . " = </td>";
+                echo "<td>" . $resto . "</td>"
+            echo "</tr>"
+
+            echo "</table>"
+        ?>
+        <?php
+            echo "</br></br>";
+            echo "<a href='index.php'>voltar</a>"
+        ?>
+    </body>
+</html>
